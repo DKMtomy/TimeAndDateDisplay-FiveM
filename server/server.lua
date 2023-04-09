476d5238cbc459ab3244b8a7a88d084e926d2f51
DKMtomy <102959198+DKMtomy@users.noreply.github.com>
2023-04-10 00:37:03 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,40 +1,29 @@
+local WAIT_TIME = 30000 -- 30 seconds
+
 function getTime()
-    if Config.ShowDateAndTime or (Config.ShowOnlyDate and Config.ShowOnlyTime) then
-        if Config.DayMonthYear then
-            return os.date("%d-%m-%Y at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.MonthDayYear then
-            return os.date("%m-%d-%Y at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.YearMonthDay then
-            return os.date("%Y-%m-%d at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.YearDayMonth then
-            return os.date("%Y-%d-%m at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
-        else
-            return os.date("%d-%m-%Y at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
-        end
-    elseif Config.ShowOnlyTime then
-        return os.date("%H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
+    local format
+    if Config.ShowOnlyTime then
+        format = "%H:%M"
     elseif Config.ShowOnlyDate then
-        if Config.DayMonthYear then
-            return os.date("%d-%m-%Y", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.MonthDayYear then
-            return os.date("%m-%d-%Y", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.YearMonthDay then
-            return os.date("%Y-%m-%d", os.time() + Config.TimezoneOffset * 60 * 60)
-        elseif Config.YearDayMonth then
-            return os.date("%Y-%d-%m", os.time() + Config.TimezoneOffset * 60 * 60)
-        else
-            return os.date("%d-%m-%Y", os.time() + Config.TimezoneOffset * 60 * 60)
-        end
-    else
-        print('Config.ShowDateAndTime or Config.ShowOnlyDate or Config.ShowOnlyTime must to be set to true in the config.')
-        return os.date("%d-%m-%Y at %H:%M", os.time() + Config.TimezoneOffset * 60 * 60)
+        format = Config.DayMonthYear and "%d-%m-%Y" or
+                 Config.MonthDayYear and "%m-%d-%Y" or
+                 Config.YearMonthDay and "%Y-%m-%d" or
+                 Config.YearDayMonth and "%Y-%d-%m" or
+                 "%d-%m-%Y"
+    else -- ShowDateAndTime or no config option set
+        format = Config.DayMonthYear and "%d-%m-%Y at %H:%M" or
+                 Config.MonthDayYear and "%m-%d-%Y at %H:%M" or
+                 Config.YearMonthDay and "%Y-%m-%d at %H:%M" or
+                 Config.YearDayMonth and "%Y-%d-%m at %H:%M" or
+                 "%d-%m-%Y at %H:%M"
     end
+    return os.date(format, os.time() + Config.TimezoneOffset * 60 * 60)
 end
 
 Citizen.CreateThread(function()
     while true do
         TriggerClientEvent('TimeAndDateDisplay-FiveM', -1, getTime())
-        Wait(30000)
+        Wait(WAIT_TIME)
     end
 end)
 
@@ -44,7 +33,6 @@ AddEventHandler('onResourceStart', function(resourceName)
         TriggerClientEvent('TimeAndDateDisplay-FiveM', -1, getTime())
     end
 end)
-  
 
 AddEventHandler("playerJoining", function (data)
     TriggerClientEvent('TimeAndDateDisplay-FiveM', source, getTime())
