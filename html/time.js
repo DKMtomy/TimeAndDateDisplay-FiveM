a4e374ce386a71b091e1a21e7bb982ce28526075
DKMtomy <102959198+DKMtomy@users.noreply.github.com>
2023-04-11 13:27:33 +0200

    Update time.js

--- html/time.js
+++ html/time.js
@@ -1,8 +1,14 @@
-const TimeAndDate = document.getElementById('TimeAndDate')
+const TimeAndDate = document.getElementById('TimeAndDate');
 
 window.addEventListener('message', (event) => {
-	let data = event.data
-	if(data.action == 'setTimeAndDate') {
-		TimeAndDate.innerText = data.time
-	}
-})
+  // Ensure that the event data is an object with the expected properties
+  if (typeof event.data === 'object' && event.data.action === 'setTimeAndDate' && typeof event.data.time === 'string') {
+    try {
+      // Set the text content of the TimeAndDate element to the time property
+      TimeAndDate.innerText = `${event.data.time}`;
+    } catch (error) {
+      // Handle any unexpected errors
+      console.error('Error setting time and date:', error);
+    }
+  }
+});
